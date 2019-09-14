c78f1b6dd36f7ba9c790fa80def2237e69670d6f
Nurul Hadi <n.hadi1996@gmail.com>
2019-09-14 23:33:34 +0700

    add Api gateway

---
+++ src/Happy5SocialMedia.ApiGateway/Startup.cs
@@ -0,0 +1,43 @@
+﻿using CacheManager.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using Happy5SocialMedia.Common;
+using System.IO;
+using Ocelot.Provider.Consul;
+
+namespace Happy5SocialMedia.ApiGateway
+{
+    public class Startup
+    {
+         public Startup(IHostingEnvironment env)
+        {
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath)                   
+                   .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(Configuration)
+                .AddConsul();
+        }
+
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            await app.UseOcelot();            
+        }
+    }
+}
